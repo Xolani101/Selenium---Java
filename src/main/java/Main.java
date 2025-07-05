d37127ca3cc0f65391cd11624f0391fea3ebe7f3
Xolani Maphumulo <mrxolanizn@gmail.com>
2025-07-05 05:06:30 +0200

    Interacting with elements.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,10 +21,13 @@ public class Main {
     }
 
     public static void main(String[] args) {
+
+        //Finding elements.
+
         driver.get("https://www.selenium.dev/selenium/web/formPage.html");
         System.out.println("Page title is: " + driver.getTitle());
 
-        WebElement emailTextbox = driver.findElement(By.id("working"));
+        WebElement emailTextbox = driver.findElement(By.id("working"));        
         System.out.println("Email Textbox: " + emailTextbox.getAttribute("type"));
 
         List<WebElement> radioButtons = driver.findElements(By.cssSelector("input[type='radio']"));
@@ -32,7 +35,7 @@ public class Main {
 
         WebElement bottomButton = driver.findElement(
             RelativeLocator.with(By.name("submit")).below(By.id("submit"))
-        );
+        );        
         System.out.println("Bottom Button Text: " + bottomButton.getAttribute("value"));
 
         WebElement image = driver.findElement(By.id("imageButton"));
@@ -40,6 +43,20 @@ public class Main {
 
         WebElement buttonClick = driver.findElement(By.cssSelector("input[value='Click!']"));
         System.out.println("Button Click Text: " + buttonClick.getAttribute("value"));
+
+        WebElement nameTextbox = driver.findElement(By.name("id-name1"));
+        System.out.println("Name Textbox: " + nameTextbox.getAttribute("value"));
+
+        //Interact with the elements.
+
+        emailTextbox.sendKeys("Xolani Maphumulo");
+
+        WebElement cheeseRadioButton = driver.findElement(By.id("cheese"));
+        if (!cheeseRadioButton.isSelected()) {
+            cheeseRadioButton.click();
+        }
+
+        bottomButton.click();
 
         driver.quit();
     }
